5bc5f99dc1d5f2172273cf3634650c5eb15f4701
Gautam Jain <gautamjain5558@gmail.com>
2024-09-24 17:06:14 +0530

    Implemented ky which is a fetch api wrapper which makes my fetch api more powerfull

--- src/app/(main)/_components/for-you-feed.tsx
+++ src/app/(main)/_components/for-you-feed.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import PostCard from "@/components/post-card";
+import kyInstance from "@/lib/ky";
 import type { PostData } from "@/lib/types";
 import { useQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
@@ -8,14 +9,7 @@ import { Loader2 } from "lucide-react";
 const ForYouFeed = () => {
   const query = useQuery<PostData[]>({
     queryKey: ["post-feed", "for-you"],
-    queryFn: async () => {
-      const res = await fetch("/api/posts/for-you");
-      if (!res.ok) {
-        throw new Error("Request failed");
-      }
-      const data = await res.json();
-      return data;
-    },
+    queryFn: kyInstance.get("/api/posts/for-you").json<PostData[]>,
   });
 
   if (query.status === "pending") {
