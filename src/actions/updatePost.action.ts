1112f98adf32644f891f3b61d499d97f5886923a
Gautam Jain <gautamjain5558@gmail.com>
2025-01-15 00:00:24 +0530

    Created update profile server action

---
+++ src/actions/updatePost.action.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { auth } from "@/auth";
+import { db } from "@/lib/connectDb";
+import { getUserDataSelect } from "@/lib/types";
+import { updateProfileSchema } from "@/schemas";
+import { z } from "zod";
+
+// both returning an error or throwing an error is one and the same thing.
+
+export async function updatePost(params: z.infer<typeof updateProfileSchema>) {
+  const validateFeilds = updateProfileSchema.safeParse(params);
+
+  if (!validateFeilds.success) {
+    throw new Error("Inputs validation failed");
+  }
+
+  const { bio, displayName } = validateFeilds.data;
+
+  // checking if user is logged in or not as only a logged in user can update their profile
+
+  const session = await auth();
+
+  if (session && session.user && session.user.id) {
+    const updatedUser = await db.user.update({
+      where: { id: session.user.id },
+      data: {
+        bio,
+        displayName,
+      },
+      select: getUserDataSelect(session.user.id),
+    });
+  } else {
+    throw new Error("Only logged in users can update their profile");
+  }
+}
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -14,3 +14,13 @@ export const registerFormSchema = z.object({
 export const postValidationSchema = z.object({
   content: z.string().trim().min(1, { message: "required" }),
 });
+
+// Here i am not keeping image prop in my updateProfileSchema because image upload
+// will be handled separately by uploadthing in the core.ts file route handler
+
+export const updateProfileSchema = z.object({
+  displayName: z.string().min(1, { message: "Display Name is required" }),
+  bio: z
+    .string()
+    .max(1000, { message: "Bio can be at most of 1000 characters" }),
+});
