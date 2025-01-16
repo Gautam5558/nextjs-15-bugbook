df7d7f3a9c167a5bd0d1b9d9139c870bf8f65160
Gautam Jain <gautamjain5558@gmail.com>
2025-01-16 22:24:02 +0530

    Created mutation for updating profile

--- src/actions/updateProfile.action.ts
+++ src/actions/updateProfile.action.ts
@@ -8,7 +8,9 @@ import { z } from "zod";
 
 // both returning an error or throwing an error is one and the same thing.
 
-export async function updatePost(params: z.infer<typeof updateProfileSchema>) {
+export async function updateProfile(
+  params: z.infer<typeof updateProfileSchema>,
+) {
   const validateFeilds = updateProfileSchema.safeParse(params);
 
   if (!validateFeilds.success) {
@@ -30,6 +32,8 @@ export async function updatePost(params: z.infer<typeof updateProfileSchema>) {
       },
       select: getUserDataSelect(session.user.id),
     });
+
+    return updatedUser;
   } else {
     throw new Error("Only logged in users can update their profile");
   }
---
+++ src/mutations/updateProfile.mutation.ts
@@ -0,0 +1,83 @@
+import { updateProfile } from "@/actions/updateProfile.action";
+import {
+  InfiniteData,
+  QueryFilters,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { updateProfileSchema } from "@/schemas/index";
+import z from "zod";
+import { PostsPage } from "@/lib/types";
+import { toast } from "@/hooks/use-toast";
+
+export const useUpdateProfileMutation = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: ({
+      values,
+      avatar,
+    }: {
+      values: z.infer<typeof updateProfileSchema>;
+      avatar?: File;
+    }) => {
+      return Promise.all([
+        updateProfile(values),
+        avatar && startAvatarUpload([avatar]),
+      ]);
+    },
+    onSuccess: async ([updatedUser, uploadResult]) => {
+      // we will now update the client side cache of user data after db data updation
+
+      const newAvatarUrl = uploadResult[0].serverData.avatarUrl;
+
+      const queryFilter: QueryFilters = {
+        queryKey: ["post-feed"],
+      };
+
+      await queryClient.cancelQueries(queryFilter);
+
+      queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>(
+        queryFilter,
+        // @ts-ignore
+        (oldData) => {
+          if (!oldData) {
+            return;
+          }
+
+          return {
+            pageParams: oldData.pageParams,
+            pages: oldData.pages.map((page) => {
+              return {
+                nextCursor: page.nextCursor,
+                posts: page.posts.map((post) => {
+                  if (post.user.id === updatedUser.id) {
+                    return {
+                      ...post,
+                      user: {
+                        ...updatedUser,
+                        image: newAvatarUrl,
+                      },
+                    };
+                  } else {
+                    return post;
+                  }
+                }),
+              };
+            }),
+          };
+        },
+      );
+
+      toast({ description: "Profile Updated" });
+    },
+    onError(error) {
+      console.log(error);
+      toast({
+        variant: "destructive",
+        description: "Something went Wrong, Try again",
+      });
+    },
+  });
+
+  return mutation;
+};
